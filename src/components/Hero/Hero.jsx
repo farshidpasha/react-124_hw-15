e9bbdd0c21e910cb8577aa2d04693ac8e7b7d3c7
farshid pashazadeh <farshidpashazadeh71@gmail.com>
2025-01-26 14:57:27 +0330

    feat(initial and Headline): initial project and feat headline component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeadLine from './components/HeadLine/HeadLine';
+import './hero.css';
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="container"></div>
+      <HeadLine />
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/components/Hero/components/HeadLine/HeadLine.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './headline.css';
+
+function HeadLine() {
+  return (
+    <header className="headline text-center">
+      <h1 className="headline__main-title">ایده‌های شما را به واقعیت تبدیل می‌کنیم</h1>
+      <h2 className="headline__secondary-title">سریع، زیبا و حرفه‌ای، طراحی سایت اختصاصی برای کسب‌وکار شما</h2>
+    </header>
+  );
+}
+
+export default HeadLine;
