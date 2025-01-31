1a5e2b999992f87da4d61ff0f46755aa61c07e94
farshid pashazadeh <farshidpashazadeh71@gmail.com>
2025-01-31 21:13:53 +0330

    feat(Comments): Add Comments component

--- src/components/comments/comments.jsx
+++ src/components/comments/comments.jsx
@@ -1,6 +1,37 @@
+// Import Swiper React components
 import React from 'react';
+// Import Swiper React components
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/swiper-bundle.css';
+
 import './comments.css';
+// import dataBase
+import commentsData from '../../services/db.json';
 
 export default function Comments() {
-  return <div style={{ width: '100%', height: '200px', backgroundColor: 'red' }}>Comments</div>;
+  console.log(commentsData);
+  return (
+    <div className="comments">
+      <Swiper
+        slidesPerView={'auto'}
+        centeredSlides={true}
+        spaceBetween={30}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {commentsData.map((comment) => (
+          <SwiperSlide key={comment.id} className="slide-container">
+            <p className="slide-description">{comment.description}</p>
+            <img src={comment.imageSrc} alt={comment.slug} className="slide-image" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
